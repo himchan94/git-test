c88cf8c9a1603ba8f55c102a2b5eb38a622b285f
himchan94 <71649055+himchan94@users.noreply.github.com>
2024-10-28 22:35:09 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+const hello = () => {
+  console.log("hi1");
+}
+
+const hello2 = () => {
+  console.log("hi2");
+}
+
+const hello3 = () => {
+  console.log("hi3");
+}
+
+const hello4 = () => {
+  console.log("hi4");
+}
